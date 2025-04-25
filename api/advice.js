3ff8afaf0127e6ca03b93ee79ca2fec89e694582
JiriSobek <sobek@adpontes.cz>
2025-04-25 18:31:33 +0200

    Update advice.js prompt7

--- api/advice.js
+++ api/advice.js
@@ -9,10 +9,9 @@ export default async function handler(req, res) {
     const { text } = req.body;
 
     const systemMessage = `
-Jsi zkušená a nápomocná sociální pracovnice a odbornice na individuální plánování zaměřené na člověka (person centered planing). 
-Pomáháš pracovnicím v sociálních službách formulovat užitečné a srozumitelné texty do individuálních plánů klientů.
-Dobrý individuální plán jasně a konkrétně popisuje, co klient zvládá sám a s čím potřebuje pomoc a jak konkrétně tato pomoc vypadá.
-Piš česky, přirozeně, srozumitelně a přátelsky – jako zkušená kolegyně, která chce poradit. Vyhýbej se úřednímu stylu, cizím a odborným výrazům.
+Jsi zkušená a nápomocná sociální pracovnice a odbornice na individuální plánování. 
+Pomáháš pracovnicím v sociálních službách formulovat texty do individuálních plánů klientů.
+Piš česky, přirozeně, srozumitelně a přátelsky – jako zkušená kolegyně, která chce poradit. Vyhýbej se úřednímu stylu, cizím a odborným výrazům. Používej krátké věty a styl jazyka, který dobře pochopí i člověk se základním vzděláním.
 `;
 
     const userPrompt = `
@@ -21,7 +20,7 @@ Text k posouzení:
 ${text}
 =====
 V odpovědi nejprve oceň snahu pracovnice při vytváření individuálního plánu.
-Zvaž tyto klíčové body: 
+Dobrý individuální plán jasně a srozumitelně popisuje, co klient zvládá sám a s čím potřebuje pomoc. Popisuje, jak konkrétně tato pomoc probíhá. Zvaž tyto klíčové body: 
 1. Je popsáno, co klient zvládá sám při ranní a večerní hygieně (např. umýt si ruce, obličej, vyčistit zuby)? Je konkrétně popsaná potřebná pomoc ze strany pracovnic?
 2. Je konkrétně popsáno, co zvládne klient při celkové hygieně (koupání, sprchování) a s čím potřebuje pomoc (např. pomoc při vstupu do sprchy/vany, namydlení těla, opláchnutí, osušení, mytí vlasů)? 
 3. Je popsáno, jestli klient chodí na toaletu sám nebo potřebuje pomoc pracovníků – např. pomoc s posazením na mísu, očištění po vykonání potřeby?
@@ -29,10 +28,10 @@ Zvaž tyto klíčové body:
 5. Má klient nějaká zvláštní přání nebo zvyklosti ohledně hygieny? Používá klient nějaké pomůcky (madlo, protiskluzová podložka)? 
 6. Hrozí při hygieně nějaké riziko? Pokud ano, musí být popsáno, jak mu předcházet.
 
-Pokud něco důležitého chybí nebo je příliš obecné, napiš 5–7 otázek, které pomohou text doplnit a upřesnit, jak má péče o klienta vypadat.
-Pokud z textu plyne, že klientka hygienu celkově nezvládá, neptej se co zvládá.
-Pokud je text v pořádku nebo pokud klient hygienu zvládá sám, žádné otázky nepokládej.
-Piš odpovědi v délce mavimálně 1600 znaků.
+Pokud něco důležitého chybí nebo je příliš obecné, napiš 5 otázek, které pomohou text doplnit a upřesnit.
+Pokud z textu plyne, že klient je zcela závislý na pomoci pracovnic, neptej se, co klient zvládá.
+Pokud klient něco zvládá sám (například ranní hygienu), žádné doplňující otázky nepokládej.
+Piš odpovědi v délce maximálně 1600 znaků.
 
 Odpověď napiš jako HTML. Používej <b>tučný text</b> a odrážky <ul><li>.
 `;
